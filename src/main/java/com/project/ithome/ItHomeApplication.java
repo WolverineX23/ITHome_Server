3be3faba984a54c7bb21f9a20fed8faa9a7c43e4
547036318 <547036318@qq.com>
2022-04-19 00:44:20 +0800

    init

---
+++ src/main/java/com/project/ithome/ItHomeApplication.java
@@ -0,0 +1,13 @@
+package com.project.ithome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/ithome/ItHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ithome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
